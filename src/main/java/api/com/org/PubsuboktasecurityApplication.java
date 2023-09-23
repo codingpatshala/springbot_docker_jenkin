7b57af05c94903438fcfbb1c498b7bde7fe1c465
Pranab Deka <pranabkdeka@gmail.com>
2023-09-23 23:44:05 +0530

    updated comment

--- src/main/java/api/com/org/PubsuboktasecurityApplication.java
+++ src/main/java/api/com/org/PubsuboktasecurityApplication.java
@@ -2,6 +2,9 @@ package api.com.org;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * @author Pranab Kumar Deka
+ * */
 
 @SpringBootApplication
 public class PubsuboktasecurityApplication {
